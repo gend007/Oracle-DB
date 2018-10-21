be9bc2ab1edfaa921a20e21922579f0027ecfac7
gend007 <42605557+gend007@users.noreply.github.com>
2018-10-21 17:18:31 +0900

    update

---
+++ Oracle_DB_tools/rman_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+### Oracle RMAN Backup Script
+
+sid=$1
+
+if [ -n "$sid" ]; then
+    export ORACLE_SID=$sid
+fi
+
+## RMANi Backup実行
+
+rman <<EOF
+connect target /
+RUN {
+ RECOVER COPY OF DATABASE WITH TAG daily_job;
+ BACKUP INCREMENTAL LEVEL 1 FOR RECOVER OF COPY WITH TAG daily_job DATABASE PLUS ARCHIVELOG;
+}
+exit
+EOF
